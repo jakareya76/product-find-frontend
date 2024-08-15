94f58e3a39e3899e480fd72cddc6661e97d735ea
jakareya76 <jakareya1306@gmail.com>
2024-08-15 11:52:26 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <h2>Hello World</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+        perferendis obcaecati, quas atque inventore illum reprehenderit facilis
+        sapiente dolorum suscipit dolores, voluptas corporis ratione in quis
+        modi sed nobis exercitationem?
+      </p>
+    </div>
+  );
+};
+
+export default App;
